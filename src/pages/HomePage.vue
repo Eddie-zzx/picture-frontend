11fad2efdbc20d69d2793239ed5a9c393a3fb3dd
zhengzexin <930571405@qq.com>
2025-12-07 21:37:35 +0800

    feat:项目初始化

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const msg = "Hello World!"
+</script>
+
+<style scoped>
+#homePage {
+}
+</style>
